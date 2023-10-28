6051a534ed961530d75bb6104dad3136bd5bf255
Weiqiang Zhu <75299929+zhuwq0@users.noreply.github.com>
2023-10-28 22:06:52 +0000

    update

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ git clone --recursive https://github.com/AI4EPS/QuakeFlow.git docs/lectures/code
 git clone --recursive https://github.com/AI4EPS/CCTorch.git docs/lectures/codes/QuakeFlow/CCTorch/ 
 git clone --recursive https://github.com/AI4EPS/ADHypo.git docs/lectures/codes/QuakeFlow/ADHypo/
 git clone --recursive https://github.com/AI4EPS/INVerse.git docs/lectures/codes/QuakeFlow/INVerse/
+git clone --recursive https://github.com/zhuwq0/hashpy2.git docs/lectures/codes/QuakeFlow/hashpy2/
 wget https://github.com/AI4EPS/INVerse/releases/download/inverse/sac-102.0-linux_x86_64.tar.gz -P docs/lectures/codes/QuakeFlow/INVerse/
 tar -xzf docs/lectures/codes/QuakeFlow/INVerse/sac-102.0-linux_x86_64.tar.gz -C docs/lectures/codes/QuakeFlow/INVerse/
