9bcab8423167f7450816c41a73c241524e6e7bba
Weiqiang Zhu <75299929+zhuwq0@users.noreply.github.com>
2023-10-28 07:01:40 +0000

    update

--- setup.sh
+++ setup.sh
@@ -7,6 +7,6 @@ python3 -m pip install -r requirements.txt
 git clone --recursive https://github.com/AI4EPS/QuakeFlow.git docs/lectures/codes/QuakeFlow/
 git clone --recursive https://github.com/AI4EPS/CCTorch.git docs/lectures/codes/QuakeFlow/CCTorch/ 
 git clone --recursive https://github.com/AI4EPS/ADHypo.git docs/lectures/codes/QuakeFlow/ADHypo/
-git clone --recursive https://github.com/AI4EPS/INVerse.git docs/lectures/codes/QuakeFlow/InVerse/
-wget https://github.com/AI4EPS/INVerse/releases/download/inverse/sac-102.0-linux_x86_64.tar.gz -P docs/lectures/codes/QuakeFlow/InVerse/
-tar -xzf docs/lectures/codes/QuakeFlow/InVerse/sac-102.0-linux_x86_64.tar.gz -C docs/lectures/codes/QuakeFlow/InVerse/
+git clone --recursive https://github.com/AI4EPS/INVerse.git docs/lectures/codes/QuakeFlow/INVerse/
+wget https://github.com/AI4EPS/INVerse/releases/download/inverse/sac-102.0-linux_x86_64.tar.gz -P docs/lectures/codes/QuakeFlow/INVerse/
+tar -xzf docs/lectures/codes/QuakeFlow/INVerse/sac-102.0-linux_x86_64.tar.gz -C docs/lectures/codes/QuakeFlow/INVerse/
